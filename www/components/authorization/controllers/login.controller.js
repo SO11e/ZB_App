9c36091cc7503a9dae25a4ee8b0f9dca5746a28d
Dennis <djcprinse@users.noreply.github.com>
2017-05-08 13:46:59 +0200

    Small update

--- www/components/authorization/controllers/login.controller.js
+++ www/components/authorization/controllers/login.controller.js
@@ -2,12 +2,11 @@ module.exports = function ($state, $scope, $ionicHistory, AuthorizationFactory) 
 
     $scope.$on('$ionicView.beforeEnter', function(){
         $scope.$root.hideTabs = "tabs-hide";
+        $scope.credentials = {
+            "email": "",
+            "password": ""
+        };
     });
-
-    $scope.credentials = {
-        "email": "",
-        "password": ""
-    };
 
     $scope.login = function () {
         AuthorizationFactory.login($scope.credentials).then(function (data) {
