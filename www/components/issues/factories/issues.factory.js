33aa58fccf9ef95ec5f9fe88a97354468260d9bf
BrianTimmermans <bjcetimm@avans.nl>
2017-05-29 15:02:35 +0200

    Post issue werkt

--- www/components/issues/factories/issues.factory.js
+++ www/components/issues/factories/issues.factory.js
@@ -1,6 +1,7 @@
 module.exports = function (hostname, $http, AuthorizationFactory, $ionicPopup, $translate, $cordovaFileTransfer) {
     var token = AuthorizationFactory.getAuthToken();
 
+
     function getIssues() {
         return $http.get(hostname + "/issues", { headers: { 'bearer': token } } ).then(function (response) {
 			
@@ -24,6 +25,8 @@ module.exports = function (hostname, $http, AuthorizationFactory, $ionicPopup, $
     }
 
     function postIssue(issue) {
+        var regionId = AuthorizationFactory.getUser().region._id;
+        console.log('RegionId: ' + regionId);
         console.log('Token: ' + token);
         if(issue.street === "" || issue.city === "" || issue.postalCode === "" || issue.lat === "" || issue.lng === ""){
             console.error('Not all required issue properties are set');
@@ -40,85 +43,47 @@ module.exports = function (hostname, $http, AuthorizationFactory, $ionicPopup, $
                 okText: $translate.instant('ISSUE_POST_DESCRIPTION_ERROR_ACCEPT')
             });
         } else {
-            return $http.post(hostname + "/issues", {
-                data: {    
-                    'streetName': issue.street,
-                    'place': issue.city,
-                    'postalCode': issue.postalCode,
-                    'description': issue.description,
-                    'latitude': issue.lat,
-                    'longitude': issue.lng
-                }}, { 
-                    headers: { 
-                        'bearer': token 
-            }}).then(function (response) {
-                console.log('API SUCCESS RESPONSE');
-                console.log(response.data);
+            console.log('Posting issue to API');
+            console.log('streetName: ' + issue.street);
+            console.log('city: ' + issue.city);
+            console.log('zipCode: ' + issue.postalCode);
+            console.log('description: ' + issue.description);
+            console.log('region: ' + regionId);
+            console.log('latitude: ' + issue.lat);
+            console.log('longitude: ' + issue.lng);
 
-                /*if(issue.photoPath !== ""){
-                    console.log('photoPath: ' + issue.photoPath);
-                    
-                    if(response.data._id !== null){
-                        //Variables for file transfer
-                        var server = "plaats hier de server URL";
-                        var targetPath = issue.photoPath;
-                        var trustHosts = true;
-                        var options = {};
-                        options.fileName = "hier komt de bestandsnaam";
+            var postIssue = {
+                streetName: issue.street,
+                city: issue.city,
+                zipCode: issue.postalCode,
+                description: issue.description,
+                region: regionId,
+                latitude: issue.lat,
+                longitude: issue.lng
+            };
 
-                        $cordovaFileTransfer.upload(server, targetPath, options, trustAllHosts)
-                            .then(function(result) {
-                                // Success!
-                                console.log("FILETRANSFER SUCCESS" );
-                                console.log(result);
-
-                                $ionicPopup.alert({
-                                    title: $translate.instant('ISSUE_POST_SUCCESS_TITLE'),
-                                    template: $translate.instant('ISSUE_POST_SUCCESS_EXPLANATION'),
-                                    okText: $translate.instant('ISSUE_POST_SUCCESS_ACCEPT')
-                                });
-                            }, function(err) {
-                                // Error
-                                console.log("FILETRANSFER ERROR" );
-                                console.error(err);
-
-                                $ionicPopup.alert({
-                                    title: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_TITLE'),
-                                    template: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_EXPLANATION'),
-                                    okText: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_ACCEPT')
-                                });
-                            }, function (progress) {
-                                // constant progress updates
-                                console.log("FILETRANSFER PROGRESS");
-                                console.log(progress);
+            return $http.post(hostname + '/issues', JSON.stringify(postIssue), {headers: {'bearer': token, 'Content-Type': 'application/json'}}).then(function(response){
+                console.log('API RESPONSE');
+                console.log(response);
+                if(response.status !== undefined){
+                    console.log(response.status);
+                    if(response.status === 201){
+                            $ionicPopup.alert({
+                            title: $translate.instant('ISSUE_POST_SUCCESS_TITLE'),
+                            template: $translate.instant('ISSUE_POST_SUCCESS_EXPLANATION'),
+                            okText: $translate.instant('ISSUE_POST_SUCCESS_ACCEPT')
                         });
-                    } else {
-                        console.log("Response from API had no issue _id");
-                        $ionicPopup.alert({
-                            title: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_TITLE'),
-                            template: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_EXPLANATION'),
-                            okText: $translate.instant('ISSUE_POST_PHOTO_UPLOAD_ERROR_ACCEPT')
-                        });
-                    }
-
-                    
-                } else { */
-                    $ionicPopup.alert({
-                        title: $translate.instant('ISSUE_POST_SUCCESS_TITLE'),
-                        template: $translate.instant('ISSUE_POST_SUCCESS_EXPLANATION'),
-                        okText: $translate.instant('ISSUE_POST_SUCCESS_ACCEPT')
-                    });
-                // }
-
-                return response.data;
-            }, function(error) {
+                        return response.data;
+                    } 
+                } 
                 $ionicPopup.alert({
                     title: $translate.instant('ISSUE_POST_ERROR_TITLE'),
                     template: $translate.instant('ISSUE_POST_ERROR_EXPLANATION'),
                     okText: $translate.instant('ISSUE_POST_ERROR_ACCEPT')
                 });
-                console.log('API ERROR RESPONSE');
-                console.log(error);
+                return response.data;
+            }, function(error){
+                console.error(error);
             });
         }
     }
